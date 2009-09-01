544f332f77000140d8348226508af048d6a149cd
Matt Jones <mbjones.89@gmail.com>
2009-09-01 03:59:58 +0000

    Formatting cleanup for SkaterData.java

--- src/org/jsc/client/SkaterData.java
+++ src/org/jsc/client/SkaterData.java
@@ -1,20 +1,15 @@
 package org.jsc.client;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import org.jsc.client.event.LoginSessionChangeEvent;
-import org.jsc.client.event.RosterChangeEvent;
 import org.jsc.client.event.SkatingClassChangeEvent;
 import org.jsc.client.event.SkatingClassChangeHandler;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerManager;
@@ -22,18 +17,7 @@ import com.google.gwt.user.client.Cookies;
 import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.FlexTable;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.SourcesTableEvents;
-import com.google.gwt.user.client.ui.TableListener;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * The SkaterData web application supports registration and payment for 
@@ -58,13 +42,6 @@ public class SkaterData implements EntryPoint, ValueChangeHandler {
 
     private ClassListModel sessionClassList;
     private RosterModel rosterModel;
-    private TextBox searchText = new TextBox();
-    private Button searchButton = new Button("Search");
-    private FlexTable classesTable = new FlexTable();
-    private FlexTable rosterTable = new FlexTable();
-    private ArrayList<String> rosterList = new ArrayList<String>();
-    //private Panel leftPanel;
-    //private Panel rightPanel;
     private HandlerManager eventBus;
     private SkaterRegistrationServiceAsync regService;
     private AboutDialog about;
@@ -290,151 +267,4 @@ public class SkaterData implements EntryPoint, ValueChangeHandler {
         // Make the call to the registration service.
         //regService.logout(null);
     }
-    
-    /**
-     * Create and lay out the components for the left side content
-     * Currently unused; keeping code as example of table layout and manipulation
-     */
-/*
-    private Panel createLeftPanel() {
-        VerticalPanel leftPanel = new VerticalPanel();
-
-        // Create a panel to contain the search widgets
-        HorizontalPanel searchBox = new HorizontalPanel();
-        searchBox.setHorizontalAlignment(HorizontalPanel.ALIGN_CENTER);
-        searchBox.add(searchText);
-        searchBox.add(searchButton);
-
-        final DialogBox dialogBox = createDialog();
-        searchButton.addClickHandler(new ClickHandler() {
-            public void onClick(ClickEvent event) {
-                dialogBox.center();
-                dialogBox.show();
-            }
-        });
-
-        // Create the class listing panel
-        VerticalPanel classList = new VerticalPanel();
-        classList.setHorizontalAlignment(VerticalPanel.ALIGN_CENTER);
-        classList.add(classesTable);
-
-        // Add the searchBox and classList to the leftPanel
-        leftPanel.add(new HTML("<p class=\"label\">Classes</p>"));
-        leftPanel.add(searchBox);
-        leftPanel.add(classList);
-        leftPanel.addStyleName("jsc-leftpanel");
-        populateClassesList();
-
-        return leftPanel;
-    }
-*/
-    /**
-     * Create an alert dialog to display
-     */
-/*
-    private DialogBox createDialog() {
-        // Create the dialog box
-        final DialogBox dialogBox = new DialogBox();
-        dialogBox.setText("Hide or Show the list");
-        dialogBox.setAnimationEnabled(true);
-        Button closeButton = new Button("Do it now!");
-        VerticalPanel dialogVPanel = new VerticalPanel();
-        dialogVPanel.setWidth("100%");
-        dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_CENTER);
-        dialogVPanel.add(closeButton);
-
-        closeButton.addClickHandler(new ClickHandler() {
-            public void onClick(ClickEvent event) {
-                //rightPanel.setVisible(!rightPanel.isVisible());
-                dialogBox.hide();
-            }
-        });
-
-        // Set the contents of the Widget
-        dialogBox.setWidget(dialogVPanel);
-
-        //classList.refreshClassList();
-
-        return dialogBox;
-    }
-*/
-    /**
-     * Create and lay out the widgets for the right side content
-     * Currently unused; keeping code as example of table layout and manipulation
-     */
-/*
-    private Panel createRightPanel() {
-        VerticalPanel rightPanel = new VerticalPanel();
-        // Add the class details to the right panel
-        rightPanel.add(new HTML("<p class=\"label\">Class Detail</p>"));
-        rightPanel.addStyleName("jsc-rightpanel");
-        rightPanel.add(rosterTable);
-        long classid = 1;
-        populateRoster(classid);
-
-        return rightPanel;
-    }
-
-    private void populateClassesList() {
-
-        classList.refreshClassList();
-
-        classesTable.setText(0, 0, "Class");
-        classesTable.setText(0, 1, "Coach");
-        classesTable.getRowFormatter().setStyleName(0, "classes-ListHeader");
-
-        // add the class to the list
-        Iterator it = classList.iterator();
-        while (it.hasNext()) {
-            int row = classesTable.getRowCount();
-            SessionSkatingClass sc = (SessionSkatingClass)it.next();
-            long classid = sc.getClassId();
-            classesTable.setText(row, 0, sc.getClassType());
-            classesTable.setText(row, 1, sc.getInstructorFullName());
-            // add button to remove this class from the list
-            Button removeButton = new Button(new Long(classid).toString());
-            classesTable.setWidget(row, 2, removeButton);
-        }
-        classesTable.addTableListener(new TableListener() {
-            public void onCellClicked(SourcesTableEvents sender, int row, int cell) {
-                if (cell == 2) {
-                    //classList.remove(row);
-                    classesTable.removeRow(row);
-                }
-            }
-        });
-    }
-
-    private void populateRoster(long classid) {
-
-        rosterTable.setText(0, 0, "Name");
-        rosterTable.setText(0, 1, "Email");
-        rosterTable.setText(0, 2, "Level");
-        rosterTable.getRowFormatter().setStyleName(0, "roster-ListHeader");
-
-        // add the class to the list
-        for (int x = 1; x < 20; x++) {
-            int row = rosterTable.getRowCount();
-            final String name = "Matthew B. Jones";
-            final String email = "mbjones.89@gmail.com";
-            final String level = "AS-" + x;
-
-            rosterList.add(name);
-            rosterTable.setText(row, 0, name);
-            rosterTable.setText(row, 1, email);
-            rosterTable.setText(row, 2, level);
-
-            // add button to remove this class from the list
-            Button removeClass = new Button("x");
-            removeClass.addClickHandler(new ClickHandler() {
-                public void onClick(ClickEvent event) {
-                    int removedIndex = rosterList.indexOf(name);
-                    rosterList.remove(removedIndex);
-                    rosterTable.removeRow(removedIndex+1);
-                }
-            });
-            rosterTable.setWidget(row, 3, removeClass);
-        }   
-    }
-*/
 }
