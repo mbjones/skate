2ba1e9dd399e4eddf42c9a1e5db481772e3baa5e
Matt Jones <mbjones.89@gmail.com>
2009-09-19 23:52:59 +0000

    Moved sql files to sql subdirectory.

--- jscdb-schema-update-100a1-to-100b1.sql
+++
@@ -1,5 +0,0 @@
--- memberstatus -- a view of membership reflecting payment status
-CREATE OR REPLACE VIEW memberstatus AS
- SELECT m.mid, m.pid, m.paymentid, m.season, p.paypal_status
-   FROM membership m, payment p
-  WHERE m.paymentid = p.paymentid;
