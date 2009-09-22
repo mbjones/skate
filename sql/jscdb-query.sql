87dfdbb6d7e3abc53f6ed9d199ae679245358d48
Matt Jones <mbjones.89@gmail.com>
2009-09-22 03:18:10 +0000

    Additional queries.

--- sql/jscdb-query.sql
+++ sql/jscdb-query.sql
@@ -12,4 +12,18 @@ SELECT ppl.surname, ppl.givenname, m.season, p.paypal_status
   FROM membership m, payment p, people ppl  
  WHERE m.paymentid = p.paymentid 
    AND m.pid = ppl.pid
-   AND season = '2009-2010';
+   AND season = '2009-2010'
+ ORDER BY p.paypal_status, ppl.surname, ppl.givenname;
+
+ --SELECT p.surname||','|| p.givenname||','||p.email||','||sc.classtype||','||sc.day||','||y.paypal_status
+
+ SELECT p.surname, p.givenname, p.email, sc.classtype, sc.day, y.paypal_status
+   FROM roster r, people p, payment y, sessionclasses sc
+  WHERE r.pid = p.pid 
+    AND r.paymentid = y.paymentid
+    AND r.classid = sc.classid
+    AND sc.season = '2009-2010'
+    AND sc.sessionname = '1'
+  ORDER BY y.paypal_status, sc.classtype, p.surname, p.givenname;
+
+   
